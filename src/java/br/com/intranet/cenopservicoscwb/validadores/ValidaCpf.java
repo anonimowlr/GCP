7936a5be0ae9f0e4ae8b10bb48be12cc7a72a2e6
jocimar <jocimar@localhost>
2019-09-05 23:32:25 -0300

    atualizado 05/09

--- src/java/br/com/intranet/cenopservicoscwb/validadores/ValidaCpf.java
+++ src/java/br/com/intranet/cenopservicoscwb/validadores/ValidaCpf.java
@@ -35,7 +35,7 @@ public class ValidaCpf implements Validator{
 //            return;
 //        }
         
-        if (cpf.length() != 11 || !calcularDigitoVerificador(cpf.substring(0, 9)).equals(cpf.substring(9, 11)) || cpf.equals("11111111111")|| cpf.equals("22222222222")|| cpf.equals("33333333333")|| cpf.equals("44444444444")|| cpf.equals("55555555555")|| cpf.equals("66666666666")|| cpf.equals("77777777777")|| cpf.equals("88888888888")|| cpf.equals("99999999999")) {
+        if (cpf.length() != 11 || !calcularDigitoVerificador(cpf.substring(0, 9)).equals(cpf.substring(9, 11)) ||cpf.equals("00000000000")|| cpf.equals("11111111111")|| cpf.equals("22222222222")|| cpf.equals("33333333333")|| cpf.equals("44444444444")|| cpf.equals("55555555555")|| cpf.equals("66666666666")|| cpf.equals("77777777777")|| cpf.equals("88888888888")|| cpf.equals("99999999999")) {
             throw new ValidatorException(
                     new FacesMessage(FacesMessage.SEVERITY_ERROR, "CPF inválido.", "Favor informar um CPF válido." ));
         }  
